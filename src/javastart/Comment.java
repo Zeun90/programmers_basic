0f32bb66640199a191a084519caf4a69be107424
Lee JiEun <gsilver190@gmail.com>
2024-06-17 15:49:48 +0900

    주석문

---
+++ src/javastart/Comment.java
@@ -0,0 +1,8 @@
+package javastart;
+
+public class Comment {
+    public static void main(String[] args) {
+        // 이 부분을 주석처리 해서 프로그램이 정상적으로 실행되도록 만들어 보세요.
+                System.out.println("HelloWorld");
+    }
+}
